708c9e10e5c3ed574a5fb2a5f6e8aaf84cad8124
Juan Andres Torres <juan.torresr@ucuenca.edu.ec>
2022-11-23 15:04:57 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 
-let numpoints = 500;
-let numPoints = [1];
+let numPoints = 20;
+let points = [];
     
 
 function setup() {
-    createCanvas (windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
     noCursor();
 
     for (let i=0; i<numPoints; i++) {
-        let point = createVector (width/2, height /2);
-        points.push (point);
+        let point = createVector(width/2, height/2);
+        points.push(point);
     }
 }
 
 function draw() {
-    background (50);
+    background(50);
 
-    if (mouseX !== pmouseX && mouseY !== pmouseY) {
+    if(mouseX !== pmouseX && mouseY !== pmouseY) {
         points.shift();
 
-        let point = createVector (mousex, mouseY) ;
-        points.push (point);
+        let point = createVector(mousex, mouseY);
+        points.push(point);
     }
 
     strokeWeight (5);
     strokeJoin(ROUND);
     noFil1();
 
-    let c1 = color('rgb (255, 150,0) ');
+    let c1 = color('rgb (255, 150,0)');
     let c2 = color('rgb (0, 150,255)');
 
     for (let i=1; i<points. length; i++) {
 
-        let howFar = map(i, 1,points. length, )
+        let howFar = map(i, 1,points.length,0,1);
+        let c = lerpColor(c1, c2, howFar);
+        stroke(c);
 
-        let prev = points [i-1];
-        let curr = points [i];
+        let prev = points[i-1];
+        let curr = points[i];
         line (prev.x, prev.y, curr.x, curr.y) ;
     }
 }
